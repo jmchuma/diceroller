a7ddeebecb030c1b0e68380feeafa6b78f689165
? Chumo Mata <jmchuma@gmail.com>
2020-10-10 12:55:50 +0200

    Add a dice image
    * activity_main.xml now has an ImageView to show the image of a dice.
    * Roll and count up functionality updated to change the dice image when
      necessary.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -20,16 +21,51 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         //Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG).show()
         val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = (1..6).random().toString()
+        val diceValue = (1..6).random()
+        resultText.text = diceValue.toString()
+
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
+        when(diceValue) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            else -> diceImage.setImageResource(R.drawable.dice_6)
+        }
     }
 
     private fun countUp() {
+        val diceImage: ImageView = findViewById(R.id.dice_image)
         val resultText: TextView = findViewById(R.id.result_text)
 
-        resultText.text = when(resultText.text) {
-            "Hello World!" -> "1"
-            "6" -> "6"
-            else -> (resultText.text.toString().toInt()+1).toString()
+        when(resultText.text.toString()) {
+            "Hello World!" -> {
+                resultText.text = "1"
+                diceImage.setImageResource(R.drawable.dice_1)
+            }
+            "1" -> {
+                resultText.text = "2"
+                diceImage.setImageResource(R.drawable.dice_2)
+            }
+            "2" -> {
+                resultText.text = "3"
+                diceImage.setImageResource(R.drawable.dice_3)
+            }
+            "3" -> {
+                resultText.text = "4"
+                diceImage.setImageResource(R.drawable.dice_4)
+            }
+            "4" -> {
+                resultText.text = "5"
+                diceImage.setImageResource(R.drawable.dice_5)
+            }
+            "5" -> {
+                resultText.text = "6"
+                diceImage.setImageResource(R.drawable.dice_6)
+            }
+            else -> {}
         }
     }
 }
