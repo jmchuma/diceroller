d96fe2d584abf61fc9e4bca95223dd94f5e36f1f
? Chumo Mata <jmchuma@gmail.com>
2020-10-17 01:58:39 +0200

    Fix count up
    - Remove result_text TextView.
    - Count up with both dices idependently.
    - Update version code.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +15,6 @@ class MainActivity : AppCompatActivity() {
        instead.
        Made it private because AndroidStudio said I could
     */
-    lateinit private var resultText: TextView
     lateinit private var diceImage1: ImageView
     lateinit private var diceImage2: ImageView
 
@@ -24,44 +22,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        resultText = findViewById(R.id.result_text)
         diceImage1 = findViewById(R.id.dice_image1)
-        diceImage2 = findViewById(R.id.dice_image2)
+        diceImage1.setTag(0)
 
-        //val rollButton: Button = findViewById(R.id.roll_button)
-        //rollButton.setOnClickListener(){rollDice()}
+        diceImage2 = findViewById(R.id.dice_image2)
+        diceImage2.setTag(0)
+
         findViewById<Button>(R.id.roll_button).setOnClickListener(){rollDice()}
         findViewById<Button>(R.id.countup_button).setOnClickListener(){countUp()}
     }
 
     private fun rollDice() {
-        //Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG).show()
-        /*val diceValue = (1..6).random()
-        resultText.text = diceValue.toString()
+        var drawableID = getDrawableDice()
+        diceImage1.setImageResource(drawableID)
+        diceImage1.setTag(drawableID)
 
-        when(diceValue) {
-            1 -> diceImage.setImageResource(R.drawable.dice_1)
-            2 -> diceImage.setImageResource(R.drawable.dice_2)
-            3 -> diceImage.setImageResource(R.drawable.dice_3)
-            4 -> diceImage.setImageResource(R.drawable.dice_4)
-            5 -> diceImage.setImageResource(R.drawable.dice_5)
-            else -> diceImage.setImageResource(R.drawable.dice_6)
-        }*/
-
-        // TODO is this better?
-        /*val drawableDice = when(diceValue) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-
-        diceImage.setImageResource(drawableDice)*/
-
-        diceImage1.setImageResource(getDrawableDice())
-        diceImage2.setImageResource(getDrawableDice())
+        drawableID = getDrawableDice()
+        diceImage2.setImageResource(drawableID)
+        diceImage2.setTag(drawableID)
     }
 
     private fun getDrawableDice(): Int {
@@ -76,32 +54,37 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun countUp() {
-        /*when(resultText.text.toString()) {
-            "Hello World!" -> {
-                resultText.text = "1"
-                diceImage.setImageResource(R.drawable.dice_1)
+        setDice(diceImage1)
+        setDice(diceImage2)
+    }
+
+    private fun setDice(dice : ImageView) {
+        when(dice.tag) {
+            0 -> {
+                dice.tag = R.drawable.dice_1
+                dice.setImageResource(R.drawable.dice_1)
             }
-            "1" -> {
-                resultText.text = "2"
-                diceImage.setImageResource(R.drawable.dice_2)
+            R.drawable.dice_1 -> {
+                dice.tag = R.drawable.dice_2
+                dice.setImageResource(R.drawable.dice_2)
             }
-            "2" -> {
-                resultText.text = "3"
-                diceImage.setImageResource(R.drawable.dice_3)
+            R.drawable.dice_2 -> {
+                dice.tag = R.drawable.dice_3
+                dice.setImageResource(R.drawable.dice_3)
             }
-            "3" -> {
-                resultText.text = "4"
-                diceImage.setImageResource(R.drawable.dice_4)
+            R.drawable.dice_3 -> {
+                dice.tag = R.drawable.dice_4
+                dice.setImageResource(R.drawable.dice_4)
             }
-            "4" -> {
-                resultText.text = "5"
-                diceImage.setImageResource(R.drawable.dice_5)
+            R.drawable.dice_4 -> {
+                dice.tag = R.drawable.dice_5
+                dice.setImageResource(R.drawable.dice_5)
             }
-            "5" -> {
-                resultText.text = "6"
-                diceImage.setImageResource(R.drawable.dice_6)
+            R.drawable.dice_5 -> {
+                dice.tag = R.drawable.dice_6
+                dice.setImageResource(R.drawable.dice_6)
             }
             else -> {}
-        }*/
+        }
     }
 }
